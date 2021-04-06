5ab3ed56e946a4fa924a040fd1ebab3a4e17e1b3
hiteshkumar848 <31490719+hiteshkumar848@users.noreply.github.com>
2021-04-06 17:54:07 +0530

    Update workerpool.go
    too many concurrent writes in console, fixed that by sending logs to a channel with fixed buffer size

--- workerpool.go
+++ workerpool.go
@@ -189,7 +189,7 @@ func (cnw *ConcreteNodeWorker) recovery(ctx CnvContext, caller string) {
 		fmt.Println("recovered:", r, caller)
 		debug.PrintStack()
 	} else {
-		fmt.Println("no recovery needed:", r, caller)
-
+		ctx.SendLog(3, fmt.Sprintf("Worker:[%s] no recovery needed for Executor:[%s] recovered:[%v] caller:[%s]",
+			cnw.Name, cnw.Executor.GetUniqueIdentifier(), r, caller), nil)
 	}
 }
